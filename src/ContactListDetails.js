cf3fe99094112f0f08a643f7c3aabf46c6f9ebae
rutvij-fsd <rutvijpatel414@gmail.com>
2022-11-04 18:52:26 +0530

    Done

--- src/ContactListDetails.js
+++ src/ContactListDetails.js
@@ -26,13 +26,7 @@ const ContactListDetails = ({ contactList, onEditHandler , onDeleteHandle }) => 
       </div>
       
     </div>
-    // <div className="conatiner ">
-    //   <ul style={{ listStyle: "none" }} className="row mt-3">
-    //     <li className="col-6 text-center fs-4 ms-5">{contactList.name}</li>
-    //     <button className="col-auto btn btn-secondary me-2">Edit</button>
-    //     <button className="col-auto btn btn-danger h-75">Delete</button>
-    //   </ul>
-    // </div>
+    
   );
 };
 
